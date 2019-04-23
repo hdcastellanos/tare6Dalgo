8b5176ffb9cea0b6034330359a84b69eb5cf43d9
HECTOR DAVID CASTELLANOS MUÐOZ <hd.castellanos@sis.virtual.uniandes.edu.co>
2019-04-23 15:20:33 -0500

    avances

--- Tarea 6/src/Parte2/bfs.java
+++ Tarea 6/src/Parte2/bfs.java
@@ -6,61 +6,90 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import sun.misc.Queue;
+
 
 public class bfs {
 
-	
-	class Nodo implements Comparable<Nodo>{
+
+	static class Node
+	{
 		int id;
 		boolean marcado;
-		List<Nodo> nodos = new ArrayList<>();
-		public Nodo(int id) {
-			this.id = id;
-		}
-		
-		public boolean marcado()
-		{
-			return marcado;
-		}
-		
-		public void setMarcado(boolean marcado)
-		{
-			this.marcado = marcado;
-		}
+		List<Node> vecinos;
 
-		@Override
-		public int compareTo(Nodo o) {
-			// TODO Auto-generated method stub
-			return 0;
+		Node(int pId)
+		{
+			this.id=pId;
+			this.vecinos = new ArrayList<>();
+
+		}
+		public void addVecino(Node pVecino)
+		{
+			this.vecinos.add(pVecino);
+		}
+		public List<Node> getNeighbours() {
+			return vecinos;
+		}
+		public void setNeighbours(List<Node> neighbours) {
+			this.vecinos = neighbours;
+		}
+		public int getId () {
+			return this.id;
 		}
 	}
-	
-	static String RUTA = "./data/NoDirected.txt";
-	
-	public static void main (String args[]) throws Exception {
-		
-		List<String> numeros = new ArrayList<>();
-		
 
-		try {
-			FileReader reader = new FileReader(RUTA);
-			BufferedReader in = new BufferedReader(reader);
-			String line = in.readLine();
-			for( int i = 0; line != null; i++)
-			{
-				numeros.add(line);
-				line = in.readLine();
+
+		static String RUTA = "./data/NoDirected.txt";
+
+		public static void main (String args[]) throws Exception {
+             Queue<Node> nodes = new Queue<>();
+			List<String> numeros = new ArrayList<>();
+            List<Node> nodos = new ArrayList<>(); 
+
+			try {
+				FileReader reader = new FileReader(RUTA);
+				BufferedReader in = new BufferedReader(reader);
+				String line = in.readLine();
+				for( int i = 0; line != null; i++)
+				{
+					numeros.add(line);
+					Node nodo = new Node(i); 
+					nodos.add(nodo);
+					line = in.readLine();
+				}
+
+				int matriz[][] = new int [numeros.size()][numeros.size()];
+				for (int i = 0; i < numeros.size() ; i++) {
+					String parte = numeros.get(i);
+					String [] num = parte.split(" ");
+
+
+					for (int j = 0; j < numeros.size() ; j++) {
+
+						matriz [i][j] = Integer.parseInt(num[j]);
+					}
+				}
+				
+				
+
+				
+				
+
+
+
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+
 			}
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		int matriz[][] = new int [numeros.size()][numeros.size()];
-		
-		
-		
-		
+
 	}
-	
-}
+
+
+
+
+
+
+
